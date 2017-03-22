bf6d78557b6e7c89a8820735b5ecb00648e71eb6
Eugeny Burnashov <shamcode@gmail.com>
2017-03-22 23:24:24 +0700

    Use simplify from external package

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,3 +1,4 @@
+import simplify from 'logic-expression-simplify';
 import Input from '../widgets/input';
 import Form from '../widgets/form';
 import Label from 'sham-ui-label-widget';
@@ -7,39 +8,6 @@ export default function() {
     let simplifiedExpresion = '';
 
     const input = new Input( '#input-text', 'input' );
-
-    function simplify( expression ) {
-        var result = '', // Result for calculation expression
-            value = [ false, true ],
-            expressionInLowerCase = expression.toLowerCase(),
-            localResult;
-
-        for (var i = 0; i < 2; i++) {
-            for (var j = 0; j < 2; j++) {
-                localResult = eval(expressionInLowerCase.replace(/a(?!lse)/g, value[i]).replace(/b/g, value[j]));
-                result += !!localResult ? '1' : '0';
-            }
-        }
-
-        return {
-            '0000': 'false',
-            '0001': 'a && b',
-            '0010': 'a && !b',
-            '0011': 'a',
-            '0100': '!a && b',
-            '0101': 'b',
-            '0110': 'a != b',
-            '0111': 'a || b',
-            '1000': '!(a && b)',
-            '1001': 'a == b',
-            '1010': '!b',
-            '1011': 'a || !b',
-            '1100': '!a',
-            '1101': '!a || b',
-            '1110': '!(a && b)',
-            '1111': 'true'
-        }[result];
-    };
 
     new Form( 'main > form', 'form', {
         onSubmit( evt ) {
